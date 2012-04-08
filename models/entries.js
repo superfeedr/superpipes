05f6b0afe906fe358c7e6025425fea0764a49263
julien <julien.genestoux@gmail.com>
2012-04-08 11:14:28 +0200

    adjusting the way entries are stored so that we never run out of space. We store at most 100 entries per feed

--- models/entries.js
+++ models/entries.js
@@ -8,16 +8,41 @@ if(rtg.auth) {
     redis.auth(rtg.auth.split(":")[1]); // Auth.
 }
 
+//
+// This is custome to the redistogo datastore
+// Entries are stored:
+// - thru their key in a zset for each agregate feed
+// - as a hash
+// We have to setup a very late expiration key for all hashes
+// since redistogo uses volatile-lru as maxmemory-policy
+// this way, when we run out of memory
+// We delete the entries in the very distant future first.
+// By default, zset can store up to 100 entries each. 
+// That should be plenty and should avoid that we bump into too many isues.
+// TODO: how many agregate feeds can we store in a 5 MB datatbase?
+
 module.exports = {
     save: function(feed, entry, callback) {
         var flat = flattener.flatten(entry);
-        redis.hmset("e:" + feed + ':' + entry.id, flat, function(err, result) {
+        var entryKey = ["e", feed, entry.id].join(":");
+        redis.hmset(entryKey, flat, function(err, result) {
             if(err) {
                 callback(err, result)
             }
             else {
-                redis.zadd("f:" + feed , entry.postedTime, entry.id, function(err, result) {
+                // Stored the key successfuly. Let's put it a very late expiration (10 years). Please note that 
+                // The actual expiration doesn't matter, as they will never expire, but be deleted by redis's volatile-lru 
+                // policy
+                redis.expire(entryKey, 60 * 60 * 24 * 365 * 10);
+                // And also store the key in the feed's zset
+                var feedKey = ["f", feed].join(":");
+                redis.zadd(feedKey , entry.postedTime, entry.id, function(err, result) {
                     callback(err, result);
+                    // We also need to truncate the zset to only store 100 entries at most.
+                    // 
+                    redis.zremrangebyrank(feedKey, -1000,  -101, function(err, result) {
+                        // Ok done.
+                    }
                 });
             }
         });
